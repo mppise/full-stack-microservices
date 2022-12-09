f66cbb05a51d5455b5c8787b6d7bf6245de0c9df
Mangesh Pise <mppise@gmail.com>
2022-12-09 10:55:03 -0500

    Kyma deployment on SAP BTP

--- start.sh
+++ start.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-clear
-../startDashboard.sh
+echo Setting up KUBECONFIG for Kyma on SAP BTP
+export KUBECONFIG=~/Code/FullstackMicroservices/4.kyma/kubeconfig.yaml
+
+# clear
+# ../startDashboard.sh
 
 kubectl apply -f 3.k8s/common/config.yml
 kubectl get configmaps
 kubectl apply -f 3.k8s/common/secret.yml
 kubectl get secrets
-kubectl apply -f 3.k8s/common/ingress.yml
+# kubectl apply -f 3.k8s/common/ingress.yml
+# kubectl get ingress
+kubectl apply -f 3.k8s/common/kyma-apirules.yml
 kubectl get ingress
 
 kubectl apply -f 3.k8s/db/mongo-volumes.yml
@@ -15,16 +20,16 @@ kubectl get pv
 kubectl get pvc 
 kubectl apply -f 3.k8s/db/mongo.yml
 
-for c in {0..5}
+for c in {0..0}
    do
       clear
       kubectl get pods
       echo
       echo
-      echo "Wait for $((30 - $c*5)) more seconds"
-      for (( i=5; i>0; i--)); do
+      echo "Wait for $((10 - $c*5)) more seconds"
+      for (( i=10; i>0; i--)); do
       sleep 1 &
-      printf "  .. Refreshing in $i ..\r"
+      printf "  .. Resuming in $i ..\r"
       wait
    done
 done
@@ -35,19 +40,22 @@ kubectl apply -f 3.k8s/order/
 kubectl apply -f 3.k8s/web/
 kubectl get pods 
 
-for c in {0..12}
-   do
-      clear
-      kubectl get pods
-      echo
-      echo
-      kubectl get ingress
-      echo
-      echo
-      echo "Wait for $((60 - $c*5)) more seconds"
-      for (( i=5; i>0; i--)); do
-      sleep 1 &
-      printf "  .. Refreshing in $i ..\r"
-      wait
-   done
-done
+# for c in {0..12}
+#    do
+#       clear
+#       kubectl get pods
+#       echo
+#       echo
+#       kubectl get ingress
+#       echo
+#       echo
+#       echo "Wait for $((60 - $c*5)) more seconds"
+#       for (( i=5; i>0; i--)); do
+#       sleep 1 &
+#       printf "  .. Refreshing in $i ..\r"
+#       wait
+#    done
+# done
+
+echo ................................................................................
+echo Done.
--- stop.sh
+++ stop.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
 
-clear
+echo Setting up KUBECONFIG for Kyma on SAP BTP
+export KUBECONFIG=~/Code/FullstackMicroservices/4.kyma/kubeconfig.yaml
+
+# clear
 kubectl delete -f 3.k8s/web/
 kubectl delete -f 3.k8s/order/
 kubectl delete -f 3.k8s/product/
 
 kubectl delete -f 3.k8s/db/mongo-express.yml
 kubectl delete -f 3.k8s/db/mongo.yml
-# kubectl delete -f 3.k8s/db/mongo-volumes.yml
+kubectl delete -f 3.k8s/db/mongo-volumes.yml
 
-for c in {0..5}
-   do
-      clear
-      kubectl get pods
-      echo
-      echo
-      echo "Wait for $((30 - $c*5)) more seconds"
-      for (( i=5; i>0; i--)); do
-      sleep 1 &
-      printf "  .. Refreshing in $i ..\r"
-      wait
-   done
-done
+# for c in {0..5}
+#    do
+#       clear
+#       kubectl get pods
+#       echo
+#       echo
+#       echo "Wait for $((30 - $c*5)) more seconds"
+#       for (( i=5; i>0; i--)); do
+#       sleep 1 &
+#       printf "  .. Refreshing in $i ..\r"
+#       wait
+#    done
+# done
 
 kubectl delete -f 3.k8s/common/config.yml
 kubectl get config
@@ -35,11 +38,18 @@ kubectl delete -f 3.k8s/common/ingress.yml
 kubectl get ingress
 echo
 echo
+kubectl delete -f 3.k8s/common/kyma-apirules.yml
+kubectl get ingress
+echo
+echo
 
-for (( i=5; i>0; i--)); do
-    sleep 1 &
-    printf "Dashboard will shutdown in $i seconds ...\r"
-    wait
-done
+# for (( i=5; i>0; i--)); do
+#     sleep 1 &
+#     printf "Dashboard will shutdown in $i seconds ...\r"
+#     wait
+# done
 
-pkill -f 'kubectl proxy'
+# pkill -f 'kubectl proxy'
+
+echo ................................................................................
+echo Done.
