595c2ad4bcf10cb8b96b034fdea1b469c6eddb6e
Mangesh Pise <mppise@gmail.com>
2022-12-09 20:27:47 -0500

    kyma serverless test

--- 1.middleware/hello-world/handler.js
+++ 1.middleware/hello-world/handler.js
@@ -3,7 +3,13 @@ module.exports = {
     console.log(event);
     console.log(context);
     event.extensions.response.status(200).send({
-      "context" : context
+      "context" : context,
+      "event" : {
+        "body" : event.body,
+        "data" : event.data,
+        "query" : event.query,
+        "params" : event.params,
+      }
     });
    }
  }
