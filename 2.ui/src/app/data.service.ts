cf406dd796cc161c30c6c336179c798b745d6c6b
Mangesh Pise <mppise@gmail.com>
2022-12-17 14:39:29 -0500

    deployment with new graphics

--- 2.ui/src/app/data.service.ts
+++ 2.ui/src/app/data.service.ts
@@ -9,23 +9,23 @@ export class DataService {
   constructor(private http: HttpClient) { } // constructor
 
   loadProducts() {
-    return this.http.post('https://productsapi.f976b46.kyma.ondemand.com/prepare', {}).toPromise();
+    return this.http.post('https://productsapi.d5c6411.kyma.ondemand.com/prepare', {}).toPromise();
   } // loadProducts
 
   emptyOrders() {
-    return this.http.post('https://ordersapi.f976b46.kyma.ondemand.com/prepare', {}).toPromise();
+    return this.http.post('https://ordersapi.d5c6411.kyma.ondemand.com/prepare', {}).toPromise();
   } // emptyOrders
 
   getProducts() {
-    return this.http.get('https://productsapi.f976b46.kyma.ondemand.com').toPromise();
+    return this.http.get('https://productsapi.d5c6411.kyma.ondemand.com').toPromise();
   } // getProducts
 
   getOrders() {
-    return this.http.get('https://ordersapi.f976b46.kyma.ondemand.com').toPromise();
+    return this.http.get('https://ordersapi.d5c6411.kyma.ondemand.com').toPromise();
   } // getOrders
 
   submitOrder(order) {
-    return this.http.post('https://ordersapi.f976b46.kyma.ondemand.com', order).toPromise();
+    return this.http.post('https://ordersapi.d5c6411.kyma.ondemand.com', order).toPromise();
   } // submitOrder
 
 } // DataService
