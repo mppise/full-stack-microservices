f5f00553b1295f09431dae3721eb818031714fcb
Mangesh Pise <mppise@gmail.com>
2023-02-19 20:49:34 -0500

    updated function

--- 1.middleware/ping/handler.js
+++ 1.middleware/ping/handler.js
@@ -2,7 +2,7 @@ module.exports = {
   main: function (event, context) {
     event.extensions.response.status(200).send({
       "date": new Date(),
-      "event": event || {},
+      "data": event.data || {},
       "context": context || {}
     });
   }
