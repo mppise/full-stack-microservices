1c68f7d9a1d1d53eb14418fca8701059dfcfc72d
Mangesh Pise <mppise@gmail.com>
2023-02-22 16:05:53 -0500

    env

--- 1.middleware/ping/handler.js
+++ 1.middleware/ping/handler.js
@@ -5,28 +5,17 @@ module.exports = {
     credstore.writeKey('mpp', 'key1', { "date": new Date() })
       .then((resp) => {
         console.log(resp);
-        res.status(200).send({
+        credstore.readKey('mpp', 'key1').then((resp) => console.log(resp)).catch((err) => console.log(err));
+        res.send({
           "date": new Date(),
-          "resp": resp,
-          "credstore": {
-            "username": process.env['CREDSTORE_USERNAME'],
-            "password": process.env['CREDSTORE_PASSWORD'],
-            "url": process.env['CREDSTORE_URL'],
-            "encryption": JSON.parse(process.env['CREDSTORE_ENCRYPTION'])
-          }
+          "resp": resp
         });
       })
       .catch((err) => {
         console.log(err);
-        res.status(204).send({
+        res.send({
           "date": new Date(),
-          "err": err,
-          "credstore": {
-            "username": process.env['CREDSTORE_USERNAME'],
-            "password": process.env['CREDSTORE_PASSWORD'],
-            "url": process.env['CREDSTORE_URL'],
-            "encryption": JSON.parse(process.env['CREDSTORE_ENCRYPTION'])
-          }
+          "err": err
         });
       });
   }
