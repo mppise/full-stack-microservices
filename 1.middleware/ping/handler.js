14204d6dcf87cda91acdf955c144e6a94d3e8f91
Mangesh Pise <mppise@gmail.com>
2023-02-23 21:21:34 -0500

    updates

--- 1.middleware/ping/handler.js
+++ 1.middleware/ping/handler.js
@@ -10,7 +10,8 @@ module.exports = {
           res.status(200);
           return {
             "date": new Date(),
-            "resp": resp
+            "resp": resp,
+            "data": event.data
           };
         }).catch((err) => console.log(err));
       }).catch((err) => console.log(err));
