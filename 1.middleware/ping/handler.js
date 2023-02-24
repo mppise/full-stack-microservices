5b5d0dafcd77e72fb664420174a68ae8e47e8592
Mangesh Pise <mppise@gmail.com>
2023-02-23 20:45:46 -0500

    updates

--- 1.middleware/ping/handler.js
+++ 1.middleware/ping/handler.js
@@ -4,19 +4,20 @@ module.exports = {
     const credstore = require('./credstore');
     credstore.writeKey('mpp', 'key1', { "date": new Date() })
       .then((resp) => {
-        console.log(resp);
-        credstore.readKey('mpp', 'key1').then((resp) => console.log(resp)).catch((err) => console.log(err));
-        res.send({
-          "date": new Date(),
-          "resp": resp
-        });
-      })
-      .catch((err) => {
+        console.log("write", resp);
+        credstore.readKey('mpp', 'key1').then((resp) => {
+          console.log("read", resp);
+          res.send({
+            "date": new Date(),
+            "resp": resp
+          });
+        }).catch((err) => console.log(err));
+      }).catch((err) => {
         console.log(err);
-        res.send({
-          "date": new Date(),
-          "err": err
-        });
+        // res.send({
+        //   "date": new Date(),
+        //   "err": err
+        // });
       });
   }
 }
